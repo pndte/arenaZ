6a3bec7d78c1f2a5865c9a6cb01404fb6b01fdab
pndte <baggerbig1@gmail.com>
2023-12-23 17:15:35 +0300

    add Barbarian.java, Shooter.java and Warrior.java game classes

---
+++ src/main/java/me/pndte/arenaz/core/gameclasses/IGameClass.java
@@ -0,0 +1,7 @@
+package me.pndte.arenaz.core.gameclasses;
+
+import org.bukkit.entity.Player;
+
+public interface IGameClass {
+    public void applyClass(Player player);
+}
---
+++ src/main/java/me/pndte/arenaz/core/gameclasses/Warrior.java
@@ -0,0 +1,10 @@
+package me.pndte.arenaz.core.gameclasses;
+
+import org.bukkit.entity.Player;
+
+public class Warrior implements IGameClass{
+    @Override
+    public void applyClass(Player player) {
+
+    }
+}
