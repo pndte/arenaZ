8a02ef6d8703560cb016b39b4e0204fd42526cb0
pndte <baggerbig1@gmail.com>
2023-12-23 19:55:55 +0300

    update ArenaZ.java due to arenaInfo added

--- src/main/java/me/pndte/arenaz/ArenaZ.java
+++ src/main/java/me/pndte/arenaz/ArenaZ.java
@@ -1,13 +1,20 @@
 package me.pndte.arenaz;
 
+import me.pndte.arenaz.core.ArenaInfo;
+import me.pndte.arenaz.core.listeners.PlayerListener;
+import me.pndte.arenaz.core.players.DefaultArenaPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ArenaZ extends JavaPlugin {
-
+    public ArenaInfo arenaInfo;
     @Override
     public void onEnable() {
+        arenaInfo = new ArenaInfo();
+        getServer().getPluginManager().registerEvents(new PlayerListener(arenaInfo), this);
+        for (var player : getServer().getOnlinePlayers()){
+            arenaInfo.addPlayer(new DefaultArenaPlayer(player));
+        }
         System.out.println("Started");
-        //getServer().getPluginManager().registerEvent();
     }
 
     @Override
