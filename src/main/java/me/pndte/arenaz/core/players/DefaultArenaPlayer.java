9f7dcf483347f6a240954d71d24dc8cd83494b9d
pndte <baggerbig1@gmail.com>
2023-12-23 19:58:32 +0300

    change DefaultArenaPlayer.java due to last game classes changes

--- src/main/java/me/pndte/arenaz/core/players/DefaultArenaPlayer.java
+++ src/main/java/me/pndte/arenaz/core/players/DefaultArenaPlayer.java
@@ -1,16 +1,31 @@
 package me.pndte.arenaz.core.players;
 
+import me.pndte.arenaz.core.game_classes.IGameClass;
 import org.bukkit.entity.Player;
 
-public class DefaultArenaPlayer implements IArenaPlayer {
+public class DefaultArenaPlayer implements IArenaPlayer, IGameClassifiable {
     private final Player _player;
+    private IGameClass _gameClass;
+    public DefaultArenaPlayer(Player player, IGameClass gameClass){
+        _player = player;
+        _gameClass = gameClass;
+    }
 
     public DefaultArenaPlayer(Player player) {
-        _player = player;
+        this(player, new IGameClass.EmptyGameClass());
     }
 
     @Override
-    public Player minecraftPlayer() {
-        return _player;
+    public Player minecraftPlayer() { return _player; }
+
+
+    @Override
+    public IGameClass gameClass() { return _gameClass; }
+
+    @Override
+    public void setGameClass(IGameClass gameClass) {
+        _gameClass = gameClass;
+         _player.getInventory().clear();
+        _gameClass.applyClass(_player);
     }
 }
