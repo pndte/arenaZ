43215daf7d31aa5a2c2ef4043571979d9aced8a7
pndte <baggerbig1@gmail.com>
2023-12-23 19:54:20 +0300

    update PlayerListener.java

--- src/main/java/me/pndte/arenaz/core/listeners/PlayerListener.java
+++ src/main/java/me/pndte/arenaz/core/listeners/PlayerListener.java
@@ -1,12 +1,33 @@
 package me.pndte.arenaz.core.listeners;
 
+import me.pndte.arenaz.core.ArenaInfo;
+import me.pndte.arenaz.core.players.DefaultArenaPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
+    private final ArenaInfo _arenaInfo;
+    public PlayerListener(ArenaInfo info){
+        _arenaInfo = info;
+    }
     @EventHandler
-    public void playerJoin(PlayerJoinEvent playerJoinEvent){
-//        playerJoinEvent.getPlayer()
+    public void playerJoin(PlayerJoinEvent joinEvent){
+        _arenaInfo.addPlayer(new DefaultArenaPlayer(joinEvent.getPlayer()));
+    }
+
+    @EventHandler
+    public void playerLeave(PlayerQuitEvent quitEvent){
+        var playerId = quitEvent.getPlayer().getUniqueId();
+        if (!_arenaInfo.containPlayer(playerId)) return;
+        _arenaInfo.removePlayer(playerId);
+    }
+
+    @EventHandler
+    public void stepOnPressurePlate(PlayerInteractEvent interactEvent){
+        //if (interactEvent.getAction() == Action.PHYSICAL && )
     }
 }
