c2d467e617c91932e8aa304cb45f6100561cddb4
pndte <baggerbig1@gmail.com>
2023-12-23 17:15:59 +0300

    update ArenaZ.java

---
+++ src/main/java/me/pndte/arenaz/ArenaZ.java
@@ -0,0 +1,16 @@
+package me.pndte.arenaz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArenaZ extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Started");
+        //getServer().getPluginManager().registerEvent();
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
