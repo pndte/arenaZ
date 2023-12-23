89f0fd4a74c1dfc35f03937100c61a9dd14bba2b
pndte <baggerbig1@gmail.com>
2023-12-23 17:14:50 +0300

    add low-level player listener

---
+++ src/main/java/me/pndte/arenaz/core/listeners/PlayerListener.java
@@ -0,0 +1,12 @@
+package me.pndte.arenaz.core.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+    @EventHandler
+    public void playerJoin(PlayerJoinEvent playerJoinEvent){
+//        playerJoinEvent.getPlayer()
+    }
+}
